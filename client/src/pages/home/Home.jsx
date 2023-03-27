6da4e75ec639970b92569d739d2658137c6333e4
yuri <yuri@example.com>
2023-03-27 14:30:25 +0900

    💄 Add card

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Hero } from "../../components/hero/Hero";
 import Topbar from "../../components/topber/Topbar";
-import { Container } from "@mui/material";
+import { Container, Grid } from "@mui/material";
+import PostCard from "../../components/postcard/PostCard";
 
 export const Home = () => {
   return (
@@ -9,6 +10,17 @@ export const Home = () => {
       <Topbar />
       <Container maxWidth="lg">
         <Hero />
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} md={4}>
+            <PostCard>xs=6 md=8</PostCard>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <PostCard>xs=6 md=4</PostCard>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <PostCard>xs=6 md=4</PostCard>
+          </Grid>
+        </Grid>
       </Container>
     </>
   );
