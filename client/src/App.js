4e5ca5bcbde311e9c35f88e53fbc214b1aeb854c
yuri <yuri@example.com>
2023-03-27 12:36:10 +0900

    ✅ Add Home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
-import { useState,useEffect } from 'react'
+import { Home } from './pages/home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>フロントエンド🚀</h1>
-    </div>
+    <Home />
   );
 }
 
