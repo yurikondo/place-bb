f71f9114e1b5b83b3edd1ac61bf7a508b94c7814
yuri <yuri@example.com>
2023-03-27 10:46:19 +0900

    ✨ Add API

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send("テスト投稿🚀");
-});
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("/api", (req, res) => {
   res.json({ message: "テスト投稿🚀" });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.listen(PORT, () => {
