80c087dbca5652ccca2856191504a3a9df263cc9
yuri <yuri@example.com>
2023-03-27 15:08:23 +0900

    💄 Add theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
-import { Home } from './pages/home/Home';
+import { Home } from "./pages/home/Home";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { blue } from "@mui/material/colors";
 
 function App() {
+  const theme = createTheme({
+    status: {
+      palette: { primary: blue },
+    },
+  });
+
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <Home />
+      </CssBaseline>
+    </ThemeProvider>
   );
 }
 
