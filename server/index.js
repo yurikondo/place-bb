31eb4426a8ede04c95a7ac6b8938497a02802b0b
yuri <yuri@example.com>
2023-03-29 12:45:40 +0900

    🗃️ Connect MongoDB

--- server/index.js
+++ server/index.js
@@ -5,6 +5,17 @@ const PORT = process.env.PORT || 3001;
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const mongoose = require("mongoose");
+
+//データベース接続
+mongoose
+  .connect(
+    "mongodb+srv://yuri:abc@cluster0.pinlvkk.mongodb.net/place-bb?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB接続中🚀"))
+  .catch((err) => {
+    console.log(`DB接続のエラー/server/index.js👉${err}`);
+  });
 
 //ミドルウェア
 app.use("/api/auth", authRoute);
@@ -22,5 +33,5 @@ app.get("*", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`サーバー起動中🚀 *:${PORT}`);
+  console.log(`サーバー起動中🚀${PORT}`);
 });
