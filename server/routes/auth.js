6112df5acbb747acae1c25bc47861d2ed88ad0f9
yuri <yuri@example.com>
2023-03-30 08:43:23 +0900

    ✅ Create user register API

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,25 @@
 const router = require("express").Router();
+const User = require("../../models/User");
 
-router.get("/", (req, res) => {
-  res.send("Auth ルーター🎉");
+//ユーザー登録
+router.post("/register", async (req, res) => {
+  try {
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    const user = await newUser.save();
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(500);
+    console.log(`$1のエラー👉` + err);
+  }
 });
+
+// router.get("/", (req, res) => {
+//   res.send("Auth ルーター🎉");
+// });
 
 module.exports = router;
