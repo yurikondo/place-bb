efd59b511297ee30f9d93e007cd5e18b336b0dd2
yuri <yuri@example.com>
2023-03-29 12:45:11 +0900

    🗃️ Add User schema

--- models/User.js
+++ models/User.js
@@ -1,1 +1,40 @@
-const mongoose = require()
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, min: 2, max: 20 },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 50,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    desc: {
+      type: String,
+      max: 70,
+    },
+  },
+  { timestamps: true }
+);
