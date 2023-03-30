fa688dfe35885c23fdb5d6781d62cbbaf34fd71b
yuri <yuri@example.com>
2023-03-30 12:49:42 +0900

    ✅ Add login API

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -14,7 +14,23 @@ router.post("/register", async (req, res) => {
     return res.status(200).json(user);
   } catch (err) {
     return res.status(500);
-    console.log(`$1のエラー👉` + err);
+    console.log(`ユーザー登録のエラー/auth.js👉` + err);
+  }
+});
+
+//ログイン
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(404).send("ユーザーが見つかりません❌");
+
+    const vailedPassword = req.body.password === user.password;
+    if (!vailedPassword) return res.status(400).json("パスワードが違います❌");
+
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(500);
+    console.log(`ログインのエラー/auth.js👉` + err);
   }
 });
 
