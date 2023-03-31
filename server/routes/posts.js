7b34c6f76ee4302bad27dccad96e0ae45bdb4f00
yuri <yuri@example.com>
2023-03-31 12:59:10 +0900

    ✅ Create post update API

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -12,6 +12,23 @@ router.post("/", async (req, res) => {
   }
 });
 
+//投稿を更新する
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({
+        $set: req.body,
+      });
+      return res.status(200).json("投稿の編集に成功しました🎉");
+    } else {
+      return res.status(403).json("他のユーザーの投稿は編集できません❌");
+    }
+  } catch (err) {
+    return res.status(403).json(`投稿更新のエラー👉` + err);
+  }
+});
+
 // router.get("/", (req, res) => {
 //   res.send("Post ルーター🎉");
 // });
