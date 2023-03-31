4a5e024e7f79702c9bc815bd9b2ea571341b11a1
yuri <yuri@example.com>
2023-03-31 12:45:47 +0900

    ✅ Create post API

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,19 @@
 const router = require("express").Router();
+const Post = require("../../models/Post");
 
-router.get("/", (req, res) => {
-  res.send("Post ルーター🎉");
+//投稿を作成する
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    return res.status(200).json(savedPost);
+  } catch (err) {
+    return res.status(500).json(`投稿作成のエラー👉` + err);
+  }
 });
+
+// router.get("/", (req, res) => {
+//   res.send("Post ルーター🎉");
+// });
 
 module.exports = router;
